5fb0185a8b8b07e7930cded49620f4da98f6665e
JustinLieu06 <justinlieu06@gmail.com>
2020-02-22 17:19:35 -0800

    dynamic modal

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -2,11 +2,11 @@ import React from 'react';
 import classes from './Modal.module.css';
 
 const modal = (props) => (
-  //TODO
-  //update modal component
-  //update layout component
-  //update
-  <div className={classes.Modal}>
+  <div className={classes.Modal}
+    style={{
+      transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+      opacity: props.show ? '1' : '0'
+    }}>
     {props.children}
   </div>
 );
