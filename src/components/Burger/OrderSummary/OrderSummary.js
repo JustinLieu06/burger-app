70f4102be23a09b4f2148f0bcb369ec331a69a42
JustinLieu06 <justinlieu06@gmail.com>
2020-03-09 20:17:05 -0700

    made sure not to always update order summary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -3,6 +3,7 @@ import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button';
 
 class OrderSummary extends Component {
+
   render() {
     const ingredientSummary = Object.keys(this.props.ingredients)
     .map(igKey => {
