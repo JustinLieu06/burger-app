ecd9c750d84e7a992af2ca09bc9636778de45888
JustinLieu06 <justinlieu06@gmail.com>
2020-03-16 18:20:50 -0700

    to update

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -4,6 +4,7 @@ import Button from '../../UI/Button/Button';
 
 class OrderSummary extends Component {
   //this could be a functional component, doesn't have to be a class
+  //update
   render() {
     const ingredientSummary = Object.keys(this.props.ingredients)
     .map(igKey => {
